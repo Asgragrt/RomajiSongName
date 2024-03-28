a116464d57d8fd63c48485d650600be707850dcb
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-03-28 03:17:35 -0600

    :art: Use enum instead of hardcoded value

--- Patches/LocalInfoPatch.cs
+++ Patches/LocalInfoPatch.cs
@@ -1,5 +1,6 @@
 using HarmonyLib;
 using Il2CppAssets.Scripts.Database;
+using Il2CppPeroPeroGames.GlobalDefines;
 using RomajiSongName.Managers;
 
 namespace RomajiSongName.Patches;
@@ -10,7 +11,7 @@ internal static class LocalInfoPatch
     internal static void Postfix(MusicInfo __instance, int language, ref LocalALBUMInfo __result)
     {
         if (!SettingsManager.IsEnabled
-            || language != 1) return;
+            || language != Language.english) return;
 
         if (!ModManager.RomajiNames.TryGetValue(__instance.uid, out var newName)) return;
 
