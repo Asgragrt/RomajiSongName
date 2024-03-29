54ba93fb8e1eb1727a5a17d50f3de0436a0d7518
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-03-29 17:10:01 -0600

    :bookmark: Version bump

--- Properties/MelonBuildInfo.cs
+++ Properties/MelonBuildInfo.cs
@@ -6,5 +6,5 @@ internal static class MelonBuildInfo
 
     internal const string ModAuthor = "Asgragrt";
 
-    internal const string ModVersion = "1.0.1";
+    internal const string ModVersion = "1.0.2";
 }
