3623d38bdcfadd785469f6877f901404e8174c44
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-19 18:07:49 -0600

    :bulb: Added some comments

--- Patches/FancyScrollPatch.cs
+++ Patches/FancyScrollPatch.cs
@@ -18,12 +18,15 @@ internal static class FancyScrollPatch
 
         __instance.musicFancyScrollView.onItemIndexChange += new Action<int>(i =>
         {
+            // Avoid getting stuck in an infinite loop
             if (_lastIndex == i) return;
             _lastIndex = i;
 
             var dbMusicTag = GlobalDataBase.dbMusicTag;
+            // Return if random
             if (_lastIndex == dbMusicTag.stageShowMusicCount - 1) return;
 
+            // Force update the current song's title
             var musicInfo = dbMusicTag.GetMusicInfoFromShowMusicUids(i);
             musicInfo?.GetLocal(GlobalDataBase.s_DbUi.curLanguageIndex);
             __instance.musicFancyScrollView.onItemIndexChange?.Invoke(i);
