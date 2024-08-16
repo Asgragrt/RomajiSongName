d9dc93b70f9a6d2aec40c8de1c841bdb96e2f09a
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-08-15 18:34:01 -0600

    ✨ Added option for displaying current music uid

--- Managers/SettingsManager.cs
+++ Managers/SettingsManager.cs
@@ -8,8 +8,14 @@ using static MelonBuildInfo;
 internal static class SettingsManager
 {
     private const string SettingsPath = $"UserData/{ModName}.cfg";
+    private static MelonPreferences_Entry<bool> _displayMusicUid;
 
     private static MelonPreferences_Entry<bool> _isEnabled;
+    internal static bool DisplayMusicUid
+    {
+        get => _displayMusicUid.Value;
+        set => _displayMusicUid.Value = value;
+    }
 
     internal static bool IsEnabled
     {
@@ -17,12 +23,12 @@ internal static class SettingsManager
         set => _isEnabled.Value = value;
     }
 
-
     internal static void Load()
     {
         var mainCategory = MelonPreferences.CreateCategory(ModName);
         mainCategory.SetFilePath(SettingsPath, true, false);
 
         _isEnabled = mainCategory.CreateEntry(nameof(IsEnabled), true);
+        _displayMusicUid = mainCategory.CreateEntry(nameof(DisplayMusicUid), false);
     }
 }
--- Patches/FancyScrollPatch.cs
+++ Patches/FancyScrollPatch.cs
@@ -12,23 +12,33 @@ internal static class FancyScrollPatch
 
     internal static void Postfix(PnlStage __instance)
     {
-        if (!SettingsManager.IsEnabled) return;
+        if (!SettingsManager.IsEnabled)
+            return;
 
         _lastIndex = -1;
 
         __instance.musicFancyScrollView.onItemIndexChange += new Action<int>(i =>
         {
             // Avoid getting stuck in an infinite loop
-            if (_lastIndex == i) return;
+            if (_lastIndex == i)
+                return;
             _lastIndex = i;
 
             var dbMusicTag = GlobalDataBase.dbMusicTag;
             // Return if random
-            if (_lastIndex == dbMusicTag.stageShowMusicCount - 1) return;
+            if (_lastIndex == dbMusicTag.stageShowMusicCount - 1)
+                return;
 
             // Force update the current song's title
             var musicInfo = dbMusicTag.GetMusicInfoFromShowMusicUids(i);
-            musicInfo?.GetLocal(GlobalDataBase.s_DbUi.curLanguageIndex);
+            if (musicInfo is null)
+                return;
+
+            if (SettingsManager.DisplayMusicUid)
+            {
+                MelonLoader.Melon<Main>.Logger.Msg(musicInfo.uid);
+            }
+            musicInfo.GetLocal(GlobalDataBase.s_DbUi.curLanguageIndex);
             __instance.musicFancyScrollView.onItemIndexChange?.Invoke(i);
         });
     }
